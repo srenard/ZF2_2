1a8e7059f9ee60308051f31f2574e04086f626f0
srenard <srenard@ruses.com>
2014-05-29 19:36:38 +0200

    Exemple d'utilisation de Zend\Db\Sql\Sql

--- module/Application/src/Application/Controller/DbController.php
+++ module/Application/src/Application/Controller/DbController.php
@@ -4,12 +4,27 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Db\Sql\Sql;
+
+/*
+  class dbController extends AbstractActionController {
+  public function test1Action() {
+  $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+  $res = $adapt->query('SELECT * FROM `articles`', $adapt::QUERY_MODE_EXECUTE);
+  $res = $res->current();
+  return new ViewModel(array('res' => $res));
+  }
+  }
+ */
 
 class dbController extends AbstractActionController {
     public function test1Action() {
-        $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
-        $res = $adapt->query('SELECT * FROM `articles`', $adapt::QUERY_MODE_EXECUTE);
-        $res = $res->current();
-        return new ViewModel(array('res' => $res));
+          $adapt = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+          $sql = new Sql($adapt);
+          $select = $sql->select();
+          $select-> from('articles')->where("articles_nom = 'taille-crayon'");
+          $chaineSql = $sql->getSqlStringForSqlObject($select);
+          $resultat = $adapt->query($chaineSql, $adapt::QUERY_MODE_EXECUTE);
+          return new ViewModel(array('resultat' => $resultat));
     }
 }
