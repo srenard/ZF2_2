7f704c0e97c1247c704fc090b0a4b1407d8566f5
srenard <srenard@ruses.com>
2014-06-03 23:44:59 +0200

    Formulaire clients version 1

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,7 @@ return array(
         'Application',
         'Articles',
         'Routestest',
+        'clients',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
