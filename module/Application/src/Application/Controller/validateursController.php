31c8128b11b4d7ea04d295b7170e31b5f2b74886
srenard <srenard@ruses.com>
2014-06-05 11:06:32 +0200

    Exemples de filtres OK Fichier Zend_validate en place Todo : traduction des messages de validation

--- module/Application/src/Application/Controller/validateursController.php
+++ module/Application/src/Application/Controller/validateursController.php
@@ -4,16 +4,27 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-use Zend\I18n\Validator\Alnum;
-
+use Zend\Validator\Barcode;
+use Zend\I18n\Translator\Translator;
+use Zend\Mvc\Translator as t2;
 class validateursController extends AbstractActionController {
 
     public function valideAction() {
-        $validator = new Alnum(array('allowWhiteSpace' => false));
-        $resultat = $validator->isValid("Test45");
+        $validator = new Barcode('EAN8');
+        $traducteur = new Translator();
+        $traducteur->addTranslationFile('phparray',
+                                        'traductions/Zend_Validate.php',
+                                        'application',
+                                        'fr_FR');
+        var_dump($traducteur);
+        $traducteur2 = $translate = new t2($traducteur);
+        $validator->setTranslator($traducteur2);
+        $resultat = $validator->isValid('32123457');
+        $messages = $validator->getMessages();
         return new ViewModel(
                 array(
             'resultat' => $resultat,
+            'messages' => $messages,
         ));
     }
 
